7b8a649e63f4636e02be8b6b10576b204d968e95
Alejandro Salas <asalas.wd@outlook.com>
2022-10-07 17:17:52 -0400

    Complete calculations for investment changes

--- src/InvestmentCalculator.java
+++ src/InvestmentCalculator.java
@@ -2,13 +2,21 @@ public class InvestmentCalculator {
     public static void main(String[] args) {
         double initial = 14_000;
         System.out.println("Initial investment: $" + initial);
+
         double first_change = 40;
         char first_type = '%';
 
-        percentageChange(initial, first_change, first_type);
+        double total = percentageChange(initial, first_change, first_type);
     }
 
-    static void percentageChange(double initial, double change, char type) {
-        System.out.println("I'm calling this method");
+    static double percentageChange(double initial, double change, char type) {
+        double result = 0;
+        if (type == '%'){
+            result = initial + initial*(change/100);
+        }
+
+
+        System.out.println("Current investment is: $" + result);
+        return result;
     }
 }
