fd07a8b1d71bc46d58516939b64be55b8bccdfc5
Alejandro Salas <asalas.wd@outlook.com>
2022-10-07 17:34:55 -0400

    Complete exercise with three changes to the investment

--- src/InvestmentCalculator.java
+++ src/InvestmentCalculator.java
@@ -4,9 +4,19 @@ public class InvestmentCalculator {
         System.out.println("Initial investment: $" + initial);
 
         double first_change = 40;
-        char first_type = '$';
+        char first_type = '%';
 
         double total = change(initial, first_change, first_type);
+
+        double second_change = -1_500;
+        char second_type = '$';
+
+        total = change(total, second_change, second_type);
+
+        double third_change = 12;
+        char third_type = '%';
+
+        change(total, third_change, third_type);
     }
 
     static double change(double initial, double change, char type) {
