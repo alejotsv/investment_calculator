ae694e460cf263dc5da16ba05ed1eff31dd56c86
Alejandro Salas <asalas.wd@outlook.com>
2022-10-07 17:31:06 -0400

    Add different types of change to the change method and display message with the change

--- src/InvestmentCalculator.java
+++ src/InvestmentCalculator.java
@@ -4,19 +4,32 @@ public class InvestmentCalculator {
         System.out.println("Initial investment: $" + initial);
 
         double first_change = 40;
-        char first_type = '%';
+        char first_type = '$';
 
-        double total = percentageChange(initial, first_change, first_type);
+        double total = change(initial, first_change, first_type);
     }
 
-    static double percentageChange(double initial, double change, char type) {
+    static double change(double initial, double change, char type) {
         double result = 0;
-        if (type == '%'){
+        if (type == '%') {
             result = initial + initial*(change/100);
+            if (change >= 0) {
+                System.out.println("After an increase of " + change + "%");
+            } else {
+                System.out.println("After a decrease of " + -1*change + "%");
+            }
         }
 
+        if (type == '$') {
+            result = initial + change;
+            if (change >= 0) {
+                System.out.println("After an increase of $" + change);
+            } else {
+                System.out.println("After a decrease of $" + -1*change);
+            }
+        }
 
-        System.out.println("Current investment is: $" + result);
+        System.out.println("Your current investment is: $" + result);
         return result;
     }
 }
